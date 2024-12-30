1f5076387bd42200b81020842075d1f9ebba5054
hasnanurazizah <hasnanur28@gmail.com>
2024-12-30 11:54:33 +0700

    hidden the password

--- src/main/java/test/soal1/ecommerce/model/Users.java
+++ src/main/java/test/soal1/ecommerce/model/Users.java
@@ -1,5 +1,6 @@
 package test.soal1.ecommerce.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -13,6 +14,8 @@ public class Users {
 
     private String name;
     private String email;
+
+    @JsonIgnore
     private String password;
 
     public Long getId() {
